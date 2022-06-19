5f6ec5d649cd7ff950b522e1a6a073c1fe13590a
misha <ggvbgg@list.ru>
2022-06-19 21:58:24 +0300

    ls 5 pr 13, for

---
+++ src/num13/ls5num13.java
@@ -0,0 +1,12 @@
+package num13;
+
+public class ls5num13 {
+    public static void main(String[] args) {
+        for (int i = -7; i < 12; i += 3) {
+            if (i != -1 && i != 5 && i != 11) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
