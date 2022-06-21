35b0d3693418f9dbc26dc7ecbf1e4f00a2f673ea
misha <ggvbgg@list.ru>
2022-06-21 17:04:45 +0300

    ls6num1 while

---
+++ src/ls6num1/ls6num1.java
@@ -0,0 +1,14 @@
+package ls6num1;
+
+import java.util.Scanner;
+
+public class ls6num1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        int num = scan.nextInt();
+        while (num != 0){
+
+        }
+    }
+}
