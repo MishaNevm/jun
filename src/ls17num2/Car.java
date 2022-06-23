e9a08d53e4caada56b64cd812e08af212c4ebeed
misha <ggvbgg@list.ru>
2022-06-23 20:37:08 +0300

    ls17 num2

---
+++ src/ls17num2/Car.java
@@ -0,0 +1,7 @@
+package ls17num2;
+
+public class Car {
+    public Car (){
+        System.out.println("Класс Car был создан");
+    }
+}
---
+++ src/ls17num2/ls17num2.java
@@ -0,0 +1,7 @@
+package ls17num2;
+
+public class ls17num2 {
+    public static void main(String[] args) {
+        Car car = new Car();
+    }
+}
