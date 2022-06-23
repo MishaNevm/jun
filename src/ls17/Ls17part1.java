14609164d2011ec4e1f4cd24de02f7040e670c50
misha <ggvbgg@list.ru>
2022-06-23 20:35:10 +0300

    ls17 num1

---
+++ src/ls17/Ls17part1.java
@@ -0,0 +1,8 @@
+package ls17;
+
+public class Ls17part1 {
+    public static void main(String[] args) {
+        Message one = new Message();
+        one.introduce("Василий");
+    }
+}
---
+++ src/ls17/Message.java
@@ -0,0 +1,7 @@
+package ls17;
+
+public class Message {
+    public void introduce (String name){
+        System.out.println("Привет, меня зовут " + name);
+    }
+}
