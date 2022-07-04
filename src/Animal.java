79a7633f20efc831561063d2559d4310d19a840f
misha <ggvbgg@list.ru>
2022-07-04 15:58:16 +0300

    st

--- src/Animal.java
+++ src/Animal.java
@@ -1,17 +1,21 @@
 public abstract class Animal {
     private int weight;
     private int height;
-    protected Animal (int weight, int height){
+
+    protected Animal(int weight, int height) {
         this.height = height;
         this.weight = weight;
     }
-    public int getWeight () {
+
+    public int getWeight() {
         return weight;
     }
-    public int getHeight (){
+
+    public int getHeight() {
         return height;
     }
-    public void showInfo () {
+
+    public void showInfo() {
         System.out.println(weight + "\n" + height);
     }
 }
