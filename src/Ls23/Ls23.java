7954360eda6105dc3aa469933016d2ab9293a7a8
misha <ggvbgg@list.ru>
2022-07-05 18:28:51 +0300

    io Buffer Reader + file creator

--- src/Ls23/Ls23.java
+++ src/Ls23/Ls23.java
@@ -1,25 +1,28 @@
 package Ls23;
 
 import java.io.*;
+import java.util.Arrays;
 
 public class Ls23 {
     public static void main(String[] args) {
         BufferedReader br = null;
         try {
-        File file = new File("Test.txt");
-        if (file.exists()) {
-            file.createNewFile();
-        }
-        PrintWriter pw = new PrintWriter(file);
-        pw.println("All working");
-        pw.println("Hello");
-        pw.close();
+            File file = new File("Test.txt");
+            if (file.exists()) {
+                file.createNewFile();
+            }
+            PrintWriter pw = new PrintWriter(file);
+            pw.println("All working");
+            pw.println("Hello");
+            pw.close();
 
-        br = new BufferedReader(new FileReader("Test.txt"));
-        String line;
-        while ((line = br.readLine()) != null) {
-            System.out.println(line);
-        }} catch (IOException e){
+            br = new BufferedReader(new FileReader("Test.txt"));
+            String[] line = new String[2];
+            for (int i = 0; i < line.length; i++){
+                line[i] = br.readLine();
+            }
+            System.out.println(line[0]);
+        } catch (IOException e) {
             System.out.println(e);
         } finally {
             try {
