2a7ef6ffb055d71b4872845ed154dd63c9ef9a96
misha <ggvbgg@list.ru>
2022-07-13 15:48:01 +0300

    Serializable

---
+++ src/Ls28/Person.java
@@ -0,0 +1,25 @@
+package Ls28;
+
+import java.io.*;
+
+public class Person implements Serializable {
+    public String name;
+    public int id;
+
+    public Person() {
+    }
+
+    public Person(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
+
