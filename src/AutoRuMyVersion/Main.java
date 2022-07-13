5567053ee6cb5522fd762957c229b631f0a25480
misha <ggvbgg@list.ru>
2022-07-13 22:16:25 +0300

    add commit to me in Main class

--- src/AutoRuMyVersion/Main.java
+++ src/AutoRuMyVersion/Main.java
@@ -5,7 +5,7 @@ import java.io.*;
 
 public class Main implements Serializable {
     public static void main(String[] args) {
-        // Зайди в класс юзер
+        // Зайди в класс юзер, добавь энамы с брендами, с моделями, с поколениями, движками и всем прочим
         DailyCar toyota = new DailyCar("Toyota", "GT86");
         DailyCar.outputDilyCar(toyota);
         DailyCar toyotaTest = DailyCar.inputDailyCar("Toyota","GT86",1);
